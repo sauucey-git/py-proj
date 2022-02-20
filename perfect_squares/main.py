ed5431fe87b9fc3f5db4270e013203edcdffcd56
D. Gabriel <91745370+sauucey-git@users.noreply.github.com>
2022-02-20 18:42:39 +0200

    Add files via upload

---
+++ perfect_squares/main.py
@@ -0,0 +1,19 @@
+# Simple program that finds the next perfect square
+from math import sqrt
+
+def nextPerfectSquare(n) -> int:
+    log = open("log.txt", "a")
+    
+    if n > 214748399:
+        print("Enter a smaller number.")
+        quit()
+    
+    for i in range(n, 2147483647):
+        if ((round(sqrt(i)) * round(sqrt(i))) == i) and (i > n):
+            log.write(f"Perfect square {i} bigger than {n}\n")
+            log.close()
+            return i
+        
+        
+print(nextPerfectSquare(20))
+    
